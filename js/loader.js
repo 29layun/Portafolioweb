70e988eefec72cd06855e4d9d2110a4c2ffc6494
29layun <mjimenezt003@alumno.uaemex.mx>
2023-06-30 13:40:01 -0600

    Agregando archivos de mi portafolio web

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var preloader = document.getElementById('preloader');
+    var loader = document.getElementById('loader');
+
+    preloader.style.visibility = 'hidden';
+    preloader.style.opacity = '0';
+    loader.style.visibility = 'hidden';
+    loader.style.opacity = '0';
+}
